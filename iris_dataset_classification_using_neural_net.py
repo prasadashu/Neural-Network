7620c86e796b526228a56e011dbe9b4e2d4733a2
prasadashu <ashuprasad29516@gmail.com>
2018-07-04 21:21:22 +0530

    Added arcsinh activation function

--- iris_dataset_classification_using_neural_net.py
+++ iris_dataset_classification_using_neural_net.py
@@ -26,29 +26,29 @@ def sigmoid(X, deriv = False):
 		return X * (1 - X)
 	return 1 / (1 + np.exp(-X))
 
-def arcsinh(X, deriv = False):
+def inverse_sinh(X, deriv = False):
 	if(deriv == True):
 		return 1 / (np.sqrt(1 + np.square(X)))
 	return np.arcsinh(X)
 
 for j in range(60000):
 	l0 = X_train # 90x4 matrix
-	l1 = sigmoid(np.dot(l0, syn0) + bias_0) # 90x3 matrix
-	l2 = sigmoid(np.dot(l1, syn1) + bias_1) # 90x1 matrix
+	l1 = inverse_sinh(np.dot(l0, syn0)) # 90x3 matrix
+	l2 = inverse_sinh(np.dot(l1, syn1)) # 90x1 matrix
 
 	learning_rate = 1
 
 	#Backpropagation
 	l2_error = y_train - l2 # 90x1 matrix
-	l2_delta = l2_error * sigmoid(l2, True) #90x1 matrix
+	l2_delta = l2_error * inverse_sinh(l2, True) #90x1 matrix
 	l1_error = np.dot(l2_delta, syn1.T) # 90x3 matrix
-	l1_delta = l1_error * sigmoid(l1, True) #90x3 matrix
+	l1_delta = l1_error * inverse_sinh(l1, True) #90x3 matrix
 
 	syn0 += learning_rate * (np.dot(l0.T, l1_delta))
 	syn1 += learning_rate * (np.dot(l1.T, l2_delta))
 
 #Changes are to be made here to improve the efficiency
-def model(X):
+def model_sigmoid(X):
 	for i in range(len(X)):
 		if X[i, 0] > 9.99999000e-01:
 			X[i, 0] = 2
@@ -59,8 +59,42 @@ def model(X):
 		if X[i, 0] != 2 and X[i, 0] != 0:
 			X[i, 0] = 1
 
-model(l2)
+def model_inverse_sinh(X):
+	for i in range(len(X)):
+		if X[i, 0] > 0.846:
+			X[i, 0] = 1
+	for i in range(len(X)):
+		if X[i, 0] < 0.81:
+			X[i, 0] = 0
+	for i in range(len(X)):
+		if X[i, 0] != 1 and X[i, 0] != 0:
+			X[i, 0] = 2
 
-
+#model_sigmoid(l2)
+#model_inverse_sinh(l2)
 #print(l2.T)
 #print(y_train.T)
+
+#Developing model for Training set
+a = inverse_sinh(np.dot(X_train, syn0))
+
+b = inverse_sinh(np.dot(a, syn1))
+
+b = b * (1/10)
+
+model_inverse_sinh(b)
+
+#Training accuracy
+print("Training set accuracy: ", 100 - (np.mean(np.abs(y_train - b)) * 100))
+
+#Developing model for Testing set
+a = inverse_sinh(np.dot(X_test, syn0))
+
+b = inverse_sinh(np.dot(a, syn1))
+
+b = b * (1/10)
+
+model_inverse_sinh(b)
+
+#Testing accuracy
+print("Testing set accuracy: ", 100 - (np.mean(np.abs(y_test - b)) * 100))
