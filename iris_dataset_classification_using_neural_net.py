863eb1b0825f0bf771ebe5246993c62ef8423f6c
prasadashu <ashuprasad29516@gmail.com>
2018-07-04 20:29:54 +0530

    Added arcsinh activation function

--- iris_dataset_classification_using_neural_net.py
+++ iris_dataset_classification_using_neural_net.py
@@ -6,28 +6,35 @@ iris = load_iris()
 X = iris.data
 y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 100)
 
 #Normalization
 X_train = (X_train - np.mean(X_train)) / (np.max(X_train) - np.min(X_train))
 X_test = (X_test - np.mean(X_test)) / (np.max(X_test) - np.min(X_test))
 
-y_test = y_test.reshape(60, 1)
-y_train = y_train.reshape(90, 1)
+y_test = y_test.reshape(y_test.shape[0], 1)
+y_train = y_train.reshape(y_train.shape[0], 1)
 
 np.random.seed(1)
 syn0 = np.random.random((4, 3))
+#bias_0 = np.zeros((1, 3))
 syn1 = np.random.random((3, 1))
+#bias_1 = np.zeros((1, 1))
 
 def sigmoid(X, deriv = False):
 	if(deriv == True):
 		return X * (1 - X)
 	return 1 / (1 + np.exp(-X))
 
+def arcsinh(X, deriv = False):
+	if(deriv == True):
+		return 1 / (np.sqrt(1 + np.square(X)))
+	return np.arcsinh(X)
+
 for j in range(60000):
 	l0 = X_train # 90x4 matrix
-	l1 = sigmoid(np.dot(l0, syn0)) # 90x3 matrix
-	l2 = sigmoid(np.dot(l1, syn1)) # 90x1 matrix
+	l1 = sigmoid(np.dot(l0, syn0) + bias_0) # 90x3 matrix
+	l2 = sigmoid(np.dot(l1, syn1) + bias_1) # 90x1 matrix
 
 	learning_rate = 1
 
@@ -54,6 +61,6 @@ def model(X):
 
 model(l2)
 
-'''
-print(l2.T)
-print(y_train.T)'''
+
+#print(l2.T)
+#print(y_train.T)
